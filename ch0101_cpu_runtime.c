8162e4b3beb8fc1b559374a4fec6bdb5ad775e06
BaoQiang <mailbaoqiang@gmail.com>
2017-04-27 20:30:28 +0800

    init project

---
+++ ch0101_cpu_runtime.c
@@ -0,0 +1,13 @@
+// Created by BaoQiang at 2017/4/27 20:08
+
+#include <windows.h>
+
+int main() {
+    for (; ;) {
+        for (int i = 0; i < 9600000; ++i) {
+
+        }
+        Sleep(10);
+    }
+    return 0;
+}
